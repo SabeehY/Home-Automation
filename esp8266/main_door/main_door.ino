d9b4ef760cdb7c46c8d91f7c83c8638841e9db9b
Sabeeh Yunus <sabeeh.yunus@gmail.com>
2020-02-28 18:32:53 -0500

    Update main_door.ino

--- esp8266/main_door/main_door.ino
+++ esp8266/main_door/main_door.ino
@@ -4,8 +4,8 @@
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
 
-#define WIFI_SSID "Sabeeh 2.4Ghz"
-#define WIFI_PASSWORD "Sabeeh_697"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 #define MQTT_HOST IPAddress(192, 168, 0, 100)
 #define MQTT_PORT 1883
